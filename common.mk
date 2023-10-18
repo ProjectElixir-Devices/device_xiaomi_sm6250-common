cdfd024c53bdb61b867c3f0afff736e88ff2cb25
userariii <abhattacharjee717@gmail.com>
2023-10-18 08:18:49 +0530

    sm6250-common: Switch soundtrigger v2.3 & Fixes the entry
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- common.mk
+++ common.mk
@@ -49,7 +49,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl \
     android.hardware.audio.service
 
 PRODUCT_COPY_FILES += \
